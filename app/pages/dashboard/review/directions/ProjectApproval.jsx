d39b1d0c0faf5ba8da27ccf71474045016a5c86f
daniel-williams <mail@danielwilliams.us>
2016-02-20 19:32:41 -0800

    Design review step directions for Project Approval and Skyberry Acceptance.

---
+++ app/pages/dashboard/review/directions/ProjectApproval.jsx
@@ -0,0 +1,116 @@
+import React, {PropTypes} from 'react';
+import {Row, Col} from 'react-bootstrap';
+
+import {TitleDrawer} from '../../../../components';
+import StatusBox from './StatusBox';
+import StepStatus from './StepStatus';
+
+
+export default React.createClass({
+  displayName: 'ProjectApproval',
+
+  mixins: [],
+
+  propTypes: {
+    status: PropTypes.string,
+
+    isApproved: PropTypes.bool,
+    showApproval: PropTypes.func,
+  },
+  getDefaultProps: function() {
+    return {
+      status: StepStatus.TODO,
+
+      isApproved: false,
+      showApproval: function() {},
+    };
+  },
+
+  getInitialState: function() {
+    return {
+      open: this.props.status === StepStatus.CURRENT,
+    };
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    if(nextProps.status !== this.props.status) {
+      this.setState({
+        open: nextProps.status === StepStatus.CURRENT,
+      });
+    }
+  },
+
+  render: function() {
+    return (
+      <div className='step'>
+        <TitleDrawer title={this.renderTitleBlock()} open={this.state.open} onClick={this.toggleOpen}>
+          {!this.props.isApproved
+            ? this.renderApprovalRequiredMessage()
+            : this.renderApprovalSummary()
+          }
+          <p className='mt'>Requesting changes, after project approval has been given, is possible. We recongnize that this is often times unavoidable. Please note, however, that if Skyberry has already produced final deliverables, or has started the process of creating final deliverables, additional charges may apply.</p>
+          {!this.props.isApproved && this.renderActionButtons()}
+        </TitleDrawer>
+      </div>
+    );
+  },
+  renderTitleBlock: function() {
+    return (
+      <h3 className='stack'><StatusBox status={this.props.status} /><span>Project Approval</span></h3>
+    );
+  },
+  renderActionButtons: function() {
+    const btnCssNames = 'btn mb-half ' + (this.props.isApproved ? 'btn-default' : 'btn-sky');
+    return (
+      <div className='mt'>
+        <button
+          type='button'
+          className={btnCssNames}
+          onClick={this.props.showApproval}
+          disabled={this.props.isApproved}>Approve Project<br /><i className='glyphicon glyphicon-thumbs-up' /></button>
+      </div>
+    );
+  },
+  renderApprovalRequiredMessage: function() {
+    return (
+      <p>Before final deliverables are produced, Skyberry requires a project approval. Project approval indicates no further edits are necessary or expected, and alerts Skyberry to start the process of producing your deliverables.</p>
+    );
+  },
+  renderApprovalSummary: function() {
+    var digiSig = this.renderSignature();
+    if(!digiSig) {
+      digiSig = <i className='glyphicon glyphicon-alert invalid' />;
+    }
+
+    return (
+      <div className='sel-opt mt-half' ref='selectedOption'>
+        <span className='lbl mb-half'>Approved By:</span>
+        <span className='val mb-half'>{digiSig}</span>
+        <button className='btn btn-default mb-half' onClick={this.props.showApproval} style={{marginLeft:'15px'}}>View</button>
+      </div>
+    );
+  },
+  renderSignature: function() {
+    if(!this.props.isApproved) {
+      return null;
+    }
+
+    var name = this.props.approvedByName;
+    var dt = typeof this.props.approvedDate === 'string'
+      ? new Date(this.props.approvedDate)
+      : this.props.approvedDate;
+    var digiSig = name + ', ' + (dt.getMonth() + 1) + '/' + dt.getDate() + '/' + dt.getFullYear() + ', ' + dt.getHours() + ':' + dt.getMinutes() + ':' + dt.getSeconds();
+
+    return (
+      <span className='signature'>{digiSig}</span>
+    );
+  },
+
+
+  toggleOpen: function() {
+    this.setState({
+      open: !this.state.open,
+    });
+  },
+
+});
---
+++ app/pages/dashboard/review/directions/SkyberryAcceptance.jsx
@@ -0,0 +1,70 @@
+import React, {PropTypes} from 'react';
+import {Row, Col} from 'react-bootstrap';
+
+import {TitleDrawer} from '../../../../components';
+import StatusBox from './StatusBox';
+import StepStatus from './StepStatus';
+
+
+export default React.createClass({
+  displayName: 'SkyberryAcceptance',
+
+  mixins: [],
+
+  propTypes: {
+    status: PropTypes.string,
+
+    isEditable: PropTypes.bool,
+  },
+  getDefaultProps: function() {
+    return {
+      status: StepStatus.TODO,
+
+      isEditable: false,
+    };
+  },
+
+  getInitialState: function() {
+    return {
+      open: this.props.status === StepStatus.CURRENT,
+    };
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    if(nextProps.status !== this.props.status) {
+      this.setState({
+        open: nextProps.status === StepStatus.CURRENT,
+      });
+    }
+  },
+
+  render: function() {
+
+    return (
+      <div className='step'>
+        <TitleDrawer title={this.renderTitleBlock()} open={this.state.open} onClick={this.toggleOpen}>
+          <p>Skyberry Studio has been notified that you have completed this design review and are ready to proceed.</p>
+          {this.props.isEditable && this.renderEditMessage()}
+        </TitleDrawer>
+      </div>
+    );
+  },
+  renderTitleBlock: function() {
+    return (
+      <h3 className='stack'><StatusBox /><span>Awaiting Skyberry</span></h3>
+    );
+  },
+  renderEditMessage: function() {
+    return (
+      <p className='mt'>You are free to make edits to this design review&mdash;including leaving additional directions and comments&mdash;up until the time when Skyberry accepts the design review. You may also cancel your requested "Next Step" anytime before Skyberry accepts the design review.</p>
+    );
+  },
+
+
+  toggleOpen: function() {
+    this.setState({
+      open: !this.state.open,
+    });
+  },
+
+});
