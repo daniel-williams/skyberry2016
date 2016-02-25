959cd162ae62afff03dd43c7de73eab1a43a2040
daniel-williams <mail@danielwilliams.us>
2016-02-24 19:18:38 -0800

    Adding some APIs for user settings page.

--- Web/Controllers/api/UserController.cs
+++ Web/Controllers/api/UserController.cs
@@ -2,6 +2,7 @@
 using Skyberry.Domain;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web.Http;
 using Web.Controllers.api;
@@ -47,5 +48,90 @@ namespace Web.Controllers
             }
             return NotFound();
         }
+
+        [HttpPost]
+        [Route("email")]
+        public IHttpActionResult UpdateEmail([FromBody]UpdateEmailBM model)
+        {
+            SkyberryUser user = UOW.SkyberryUsers.GetById(UserIdentity.GetUserId());
+            if(user == null)
+            {
+                return new ApiNotFoundResult(Request);
+            }
+
+            user.Email = model.email;
+            UOW.Commit();
+
+            return new ApiPayloadResult<UpdateEmailBM>(Request, model);
+        }
+
+        [HttpPost]
+        [Route("username")]
+        public IHttpActionResult UpdateUsername([FromBody]UpdateUsernameBM model)
+        {
+            SkyberryUser user = UOW.SkyberryUsers.GetById(UserIdentity.GetUserId());
+            if (user == null)
+            {
+                return new ApiNotFoundResult(Request);
+            }
+
+            user.UserName = model.username;
+            UOW.Commit();
+
+            return new ApiPayloadResult<UpdateUsernameBM>(Request, model);
+        }
+
+        [HttpPost]
+        [Route("password")]
+        public IHttpActionResult UpdatePassword([FromBody]UpdatePasswordBM model)
+        {
+            SkyberryUser user = UOW.SkyberryUsers.GetById(UserIdentity.GetUserId());
+            if (user == null)
+            {
+                return new ApiNotFoundResult(Request);
+            }
+            if(!UserManager.CheckPassword(user, model.OldPassword))
+            {
+                return new ApiBadRequestResult("Current password is incorrect.", Request);
+            }
+            if(model.NewPassword != model.ConfirmPassword)
+            {
+                return new ApiBadRequestResult("Confirm password does not match new password.", Request);
+            }
+
+            user.PasswordHash = UserManager.PasswordHasher.HashPassword(model.NewPassword);
+            UOW.Commit();
+
+            return new ApiOkeyDokeResult(Request);
+        }
+    }
+
+    public class UpdateEmailBM
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string email { get; set; }
+    }
+
+    public class UpdateUsernameBM
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string username { get; set; }
+    }
+
+    public class UpdatePasswordBM
+    {
+        [Required(ErrorMessage = "Current password is required.")]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "New password is required.")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required.")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Confirm password does not match new password.")]
+        public string ConfirmPassword { get; set; }
     }
 }
