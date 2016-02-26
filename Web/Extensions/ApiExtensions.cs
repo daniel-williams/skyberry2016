39ed54f3270a159b5d82fef2bfe8fc8ab6368c30
daniel-williams <mail@danielwilliams.us>
2016-02-25 20:58:07 -0800

    Api extension to extract ModelState errors to my liking.

---
+++ Web/Extensions/ApiExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using ModelBinding = System.Web.Http.ModelBinding;
+
+namespace Web.Extensions
+{
+
+    public static class ModelStateExtensions
+    {
+        public static Dictionary<string, IEnumerable<string>> ToErrorDictionary(
+            this ModelBinding.ModelStateDictionary modelState,
+            bool camelCaseKeyName = true
+        )
+        {
+            var errors = modelState
+                .Where(x => x.Value.Errors.Any())
+                .ToDictionary(
+                    kvp => StripModelStateKeys(kvp.Key),
+                    kvp => kvp.Value.Errors.Select(e => e.ErrorMessage)
+                );
+
+            return errors;
+        }
+
+        private static string StripModelStateKeys(string propName)
+        {
+            return propName.IndexOf('.') >= 0 ? propName.Substring(propName.LastIndexOf('.') + 1) : propName;
+
+        }
+        private static string CamelCasePropNames(string propName)
+        {
+            System.Diagnostics.Debug.Write(propName);
+            var array = propName.Split('.');
+            var camelCaseList = new string[array.Length];
+            for (var i = 0; i < array.Length; i++)
+            {
+                var prop = array[i];
+                camelCaseList[i] = prop.Substring(0, 1).ToLower() + prop.Substring(1, prop.Length - 1);
+            }
+
+            return string.Join(".", camelCaseList);
+        }
+    }
+
+}
--- Web/Filters/ValidateModelAttribute.cs
+++ Web/Filters/ValidateModelAttribute.cs
@@ -1,13 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
-using System.Linq;
-using Newtonsoft.Json;
-
-using ModelBinding = System.Web.Http.ModelBinding;
-using System.Text;
+using Web.Extensions;
 
 namespace Web.Filters
 {
@@ -27,40 +24,5 @@ namespace Web.Filters
         }
     }
 
-    public static class ModelStateExtensions
-    {
-        public static Dictionary<string, IEnumerable<string>> ToErrorDictionary(
-            this ModelBinding.ModelStateDictionary modelState,
-            bool camelCaseKeyName = true
-        )
-        {
-            var errors = modelState
-                .Where(x => x.Value.Errors.Any())
-                .ToDictionary(
-                    kvp => StripModelStateKeys(kvp.Key),
-                    kvp => kvp.Value.Errors.Select(e => e.ErrorMessage)
-                );
-
-            return errors;
-        }
-
-        private static string StripModelStateKeys(string propName)
-        {
-            return propName.IndexOf('.') >=0 ? propName.Substring(propName.LastIndexOf('.') + 1) : propName;
-            
-        }
-        private static string CamelCasePropNames(string propName)
-        {
-            System.Diagnostics.Debug.Write(propName);
-            var array = propName.Split('.');
-            var camelCaseList = new string[array.Length];
-            for (var i = 0; i < array.Length; i++)
-            {
-                var prop = array[i];
-                camelCaseList[i] = prop.Substring(0, 1).ToLower() + prop.Substring(1, prop.Length - 1);
-            }
-
-            return string.Join(".", camelCaseList);
-        }
-    }
+    
 }
