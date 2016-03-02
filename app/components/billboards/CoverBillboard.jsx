e28660ffd57eaa2a56030d648c805514ebbf33a6
daniel-williams <mail@danielwilliams.us>
2016-03-01 18:48:34 -0800

    Add overlay option to CoverBillboard (opacity and color).

--- app/components/billboards/CoverBillboard.jsx
+++ app/components/billboards/CoverBillboard.jsx
@@ -9,20 +9,58 @@ export default React.createClass({
 
   propTypes: {
     imgSrc: PropTypes.string.isRequired,
+    posX: PropTypes.number,
+    posY: PropTypes.number,
+    overlayOpacity: PropTypes.number,
+    overlayColor: PropTypes.string,
+  },
+  getDefaultProps: function() {
+    return {
+      posX: 50,
+      posY: 50,
+      overlayOpacity: 0,
+      overlayColor: '#fff',
+    };
+  },
+  hasOverlay: function() {
+    return this.props.overlayOpacity > 0;
+  },
+  getOverlay: function() {
+    return this.props.overlayOpacity === 100 ? 1.0 : `0.${this.props.overlayOpacity}`;
   },
 
   render: function() {
-    var imgSrc = this.props.imgSrc;
-    var imgUrl = 'url(' + imgSrc + ')';
-    var style = {backgroundImage: imgUrl};
+    const imgSrc = this.props.imgSrc;
+    const imgUrl = 'url(' + imgSrc + ')';
+    const style = {
+      backgroundImage: imgUrl,
+      backgroundPosition: `${this.props.posX}% ${this.props.posY}%`,
+    };
+
 
     return (
       <div className='sky cover-billboard' style={style}>
-        <Grid fluid={false}>
+        <Grid fluid={true} style={{position:'relative',zIndex:20}}>
           {this.props.children}
         </Grid>
+        {this.hasOverlay() && this.renderOverlay()}
       </div>
     );
+  },
+
+  renderOverlay: function() {
+    const style = {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: this.props.overlayColor,
+      opacity: this.getOverlay(),
+      zIndex: 10,
+    };
+
+    return <div style={style}></div>
   },
 
 });
